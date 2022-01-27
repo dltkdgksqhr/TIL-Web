b7b8aa2fdd64ab57ced983eb8e1a476a0aee85e9
dltkdgksqhr <78789ks@naver.com>
2022-01-27 20:18:37 +0900

    22/01/27 8.2.6절 redirect 방식으로 다른 서블릿에 데이터 전달하기

---
+++ pro08/src/sec01/ex04/FirstServlet.java
@@ -0,0 +1,19 @@
+package sec01.ex04;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	  response.setContentType("text/html;charset=utf-8");
+	  PrintWriter out = response.getWriter();
+	  response.sendRedirect("second?name=lee");
+  }
+
+}
