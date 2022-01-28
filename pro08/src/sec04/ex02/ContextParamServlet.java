13d92c5d762469d01b5e64220818ff047d2b07c6
dltkdgksqhr <78789ks@naver.com>
2022-01-28 18:45:35 +0900

    22/01/28 8.5.3절 ServletContext의 매개변수 설정 기능

---
+++ pro08/src/sec04/ex02/ContextParamServlet.java
@@ -0,0 +1,43 @@
+package sec04.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/initMenu")
+public class ContextParamServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)  throws ServletException, IOException{
+	     doHandle(request, response);
+	   }
+	
+	
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		  request.setCharacterEncoding("utf-8");
+	      response.setContentType("text/html;charset=utf-8");
+	      PrintWriter out=response.getWriter();
+	      ServletContext context = getServletContext();
+	      String menu_member = context.getInitParameter("menu_member");
+	      String menu_order = context.getInitParameter("menu_order");
+	      String menu_goods = context.getInitParameter("menu_goods");
+	      
+	      out.print("<html><body>");
+	      out.print("<table border=1 collspacing=0><tr>메뉴 이름</tr>");	      
+	      out.print("<tr><td>"+ menu_member + "</tr></td>");
+	      out.print("<tr><td>"+ menu_order + "</tr></td>");
+	      out.print("<tr><td>"+ menu_goods + "</tr></td>");	    
+	      out.print("</tr></table></body></html>");
+		
+		
+	}
+}
