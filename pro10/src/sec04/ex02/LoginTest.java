ca12c033924ecd854f6b3ee14bd9bbe7bc7872f4
dltkdgksqhr <78789ks@naver.com>
2022-02-04 23:29:02 +0900

    22/02/04  10.4.2절 HttpSessionListener이용해 로그인 접속자수 표시

--- pro10/src/sec04/ex02/LoginTest.java
+++ pro10/src/sec04/ex02/LoginTest.java
@@ -1,8 +1,11 @@
-package sec04.ex01;
+package sec04.ex02;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-//@WebServlet("/login8")
+@WebServlet("/login9")
 public class LoginTest extends HttpServlet {
+	ServletContext context=null;
+	List user_list = new ArrayList();
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
+		context=getServletContext();
 		PrintWriter out = response.getWriter();
 		HttpSession session=request.getSession();
 		String user_id=request.getParameter("user_id");
@@ -23,15 +29,19 @@ public class LoginTest extends HttpServlet {
 		LoginImpl loginUser=new LoginImpl(user_id, user_pw);
 		if(session.isNew()) {
 			session.setAttribute("loginUser", loginUser);
+			user_list.add(user_id);
+			context.setAttribute("user_list",user_list);
 		}
-		out.print("<head>");
-		out.print("<script>");
-		out.print("setTimeout('history.go(0);',5000)");
-		out.print("</script>");
-		out.print("</head>");
+
 		out.print("<html><body>");
 		out.print("아이디는 " +loginUser.user_id + "<br>");
 		out.print("총 접속자수는 " + LoginImpl.total_user + "<br>");
+		out.print("접속 아이디:<br>");
+		List list = (ArrayList) context.getAttribute("user_list");
+		for(int i=0; i<list.size();i++) {
+			out.println(list.get(i)+"<br>");
+		}
+		out.print("<a href='logout?user_id=" + user_id +"'>로그아웃 </a>");
 		out.print("</body>");
 		out.print("</html>");
 	}
