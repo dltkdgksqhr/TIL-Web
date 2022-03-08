6001808975fa4bb7ba697a59707f33d499066a37
dltkdgksqhr <78789ks@naver.com>
2022-03-08 23:30:18 +0900

    22/03/08  16.5.3절 XML 데이터 연동하기

--- pro16/src/sec01/ex01/AjaxTest1.java
+++ pro16/src/sec01/ex01/AjaxTest1.java
@@ -12,17 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ajaxTest1")
 public class AjaxTest1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doHandler(request, response);
+		doHandle(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doHandler(request, response);
+		doHandle(request, response);
 	}
 	
-	private void doHandler(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
-		String param =(String) request.getParameter("param");
+		String param = (String) request.getParameter("param");
 		System.out.println("param = " + param);
 		PrintWriter writer = response.getWriter();
 		writer.print("안녕하세요! 서버입니다.");
