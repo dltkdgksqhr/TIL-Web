5281ec2bfa3cc6fdd7fb0ee799c067b834248903
dltkdgksqhr <78789ks@naver.com>
2022-03-21 23:05:34 +0900

    22/03/21  17.4.2절 게시판 글 쓰기 구현 1

---
+++ pro17/src/sec03/brd02/BoardController.java
@@ -0,0 +1,132 @@
+package sec03.brd02;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+@WebServlet("/board/*")
+public class BoardController extends HttpServlet {
+	private static String ARTICLE_IMAGE_REPO = "C:\\board\\article_image"; // 글에 첨부한 이미지 저장 위치를 상수로 선언합니다.
+	BoardService boardService;
+	ArticleVO articleVO;
+	
+	public void init() {
+		boardService = new BoardService(); // BoardService 객체 생성
+		articleVO = new ArticleVO();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String nextPage ="";	
+	request.setCharacterEncoding("utf-8");
+	response.setContentType("text/html;charset=utf-8");
+	String action = request.getPathInfo(); //URL에서 요청명을 가져옵니다.
+	System.out.println("action: " + action);
+	
+	
+	try {
+		List<ArticleVO> articlesList = new ArrayList<>();
+		if(action==null) {
+			articlesList = boardService.listArticles();
+			request.setAttribute("articlesList", articlesList);
+			nextPage = "/board02/listArticles.jsp";
+		} else if(action.equals("/listArticles.do")) {  // action 값이 /listArticles.do면 전체 글을 조회합니다.
+	        articlesList = boardService.listArticles();  // 전체 글을 조회합니다.
+	        request.setAttribute("articlesList", articlesList);  //조회된 글 목록을 articlesList로 바인딩 한 후 listArticles.jsp로 포워딩합니다.
+	        nextPage = "/board02/listArticles.jsp";
+		} else if (action.equals("/articleForm.do")) { // action 값 /articleForm.do로 요청 시 글쓰기창이 나타납니다.
+			nextPage = "/board02/articleForm.jsp";
+		} else if (action.equals("/addArticle.do")) { // /addAticle.do로 요청 시 새글 추가 작업을 수행합니다.
+		  Map<String, String> articleMap;
+		  articleMap = upload(request, response); // 파일 업로드 기능을 사용하기 위해 upload()로 요청을 전달합니다.
+		  String title = articleMap.get("title");
+		  String content = articleMap.get("content");
+		  String imageFileName = articleMap.get("imageFileName"); // articleMap에 저장된 글 정보를 다시 가져옵니다.
+		  
+		  articleVO.setParentNO(0);  // 새 글의 부모 글 번호를 0으로 설정합니다.
+		  articleVO.setId("hong"); // 새 글 작성자 ID를 hong으로 설정합니다.
+		  articleVO.setTitle(title);
+		  articleVO.setContent(content);
+		  articleVO.setImageFileName(imageFileName);
+		  boardService.addArticle(articleVO); // 글쓰기창에서 입력된 정보를 article VO 객체에 설정한 후 addArticle()로 전달합니다.
+		  nextPage = "/board/listArticles.do";
+		} else {
+		  nextPage = "/board02/listArticles.jsp";
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response);
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+	} 	
+}
+
+
+private Map<String, String> upload(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    Map<String, String> articleMap = new HashMap<String, String>();
+    String encoding = "utf-8";
+    File currentDirPath = new File(ARTICLE_IMAGE_REPO);  // 글 이미지 저장 폴더에 대해 파일 객체를 생성합니다.
+    DiskFileItemFactory factory = new DiskFileItemFactory();
+    factory.setRepository(currentDirPath);  
+    factory.setSizeThreshold(1024 * 1024);  //메모리에 저장할 수 있는 최대 크기
+    ServletFileUpload upload = new ServletFileUpload(factory);
+    
+    List items;
+	try {
+		items = upload.parseRequest((RequestContext) request);
+		for(int i=0; i < items.size(); i++) {
+	    	  FileItem fileItem = (FileItem) items.get(i);
+	    	  if(fileItem.isFormField()) {
+	    		System.out.println(fileItem.getFieldName() + "=" + fileItem.getString(encoding));
+	    		articleMap.put(fileItem.getFieldName(), fileItem.getString(encoding)); // 파일 업로드로 같이 전송된 새 글 관련 매개변수를 Map에 (key, value)로 저장한 후 반환하고, 새글과 관련된 title, content를 Map에 저장합니다.
+	    	  } else {
+	    		System.out.println("파라미터명 : " + fileItem.getFieldName());
+	    		System.out.println("파일크기:" + fileItem.getSize() + "bytes");
+	    		if(fileItem.getSize() > 0) {
+	    		  int idx = fileItem.getName().lastIndexOf("\\");
+	    		    if(idx == -1) {
+	    		    	idx = fileItem.getName().lastIndexOf("/");
+	    		    }
+	    		  
+	    		  String fileName = fileItem.getName().substring(idx + 1);
+	    		  System.out.println("파일명 : " + fileName);
+	    		  articleMap.put(fileItem.getFieldName(), fileName);  // 업로드된 파일의 이름을 Map에 ("imageFileName","업로드 파일이름")로 저장합니다.
+	    		  File uploadFile = new File(currentDirPath + "\\" + fileName);
+	    		  fileItem.write(uploadFile);
+	    		} //end if
+	    	  }// end if
+	      } // end for
+	} catch (FileUploadException e) {
+		
+		e.printStackTrace();
+	}
+      
+	return articleMap;
+    }
+}
---
+++ pro17/src/sec03/brd02/BoardDAO.java
@@ -0,0 +1,120 @@
+package sec03.brd02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class BoardDAO {
+  private DataSource dataFactory;
+  Connection conn;
+  PreparedStatement pstmt;
+  
+  public BoardDAO() {
+	  Context ctx;
+	try {
+		ctx = new InitialContext();
+		Context envContext = (Context) ctx.lookup("java:/comp/env");
+		  dataFactory = (DataSource) envContext.lookup("jdbc/oracle");
+	} catch (NamingException e) {
+		e.printStackTrace();
+	}
+	  
+ }
+   public List selectAllArticles() {
+	   List articlesList = new ArrayList();
+	   try {
+		conn = dataFactory.getConnection();
+		  String query ="select level,articleNO,parentNO,title,content,id,writeDate"
+			        + " from t_board"
+			        + " start with parentNO=0" + " connect by prior articleNO=parentNO"
+			        + " order siblings by articleNO desc"; // 오라클의 계층형 SQL문을 실행합니다.
+	   System.out.println(query);
+	   pstmt = conn.prepareStatement(query);
+	   ResultSet rs = pstmt.executeQuery();
+	   while (rs.next()) {
+		int level = rs.getInt("level"); // 각 글의 깊이(계층)를 level 속성에 저장합니다.
+		int articleNO = rs.getInt("articleNO"); // 글 번호는 숫자형이므로 getInt()로 값을 가져옵니다.
+		int parentNO = rs.getInt("parentNO");
+		String title = rs.getString("title");
+		String content = rs.getString("content");
+		String id = rs.getString("id");
+		Date writeDate = rs.getDate("writeDate");
+		
+		ArticleVO article = new ArticleVO();  // 글 정보를 ArticleVO 객체 속성에 설정합니다.
+		article.setLevel(level);
+		article.setArticleNO(articleNO);
+		article.setParentNO(parentNO);
+		article.setTitle(title);
+		article.setContent(content);
+		article.setId(id);
+		article.setWriteDate(writeDate);
+		articlesList.add(article);
+	   }
+	   rs.close();
+	   pstmt.close();
+	   conn.close();
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}
+	return articlesList;
+	 
+   }
+   
+   private int getNewArticleNO() {
+	try {
+		conn = dataFactory.getConnection();
+		String query = "select max(articleNO) from t_board "; // 기본 글 번호 중 가장 큰 번호를 조회합니다.
+		System.out.println(query);
+		pstmt = conn.prepareStatement(query);
+		ResultSet rs =pstmt.executeQuery();
+		if (rs.next()) 
+		  return (rs.getInt(1) + 1); // 가장 큰 번호에 1을 더한 번호를 반환합니다.
+	    rs.close();
+	    pstmt.close();
+	    conn.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+	return 0;
+   }
+  
+   public void insertNewArticle(ArticleVO article) {
+	 try {
+		conn = dataFactory.getConnection();
+		int articleNO = getNewArticleNO(); // 새 글을 추가하기 전에 새 글에 대한 글 번호를 가져옵니다.
+		 int parentNO = article.getParentNO();
+		 String title = article.getTitle();
+		 String content = article.getContent();
+		 String id = article.getId();
+		 String imageFileName = article.getImageFileName();
+		 String query = "insert into t_board (articleNO, parentNO, title, content, imageFileName, id)" +
+		                " values (?, ?, ?,? ,?)";
+		 System.out.println(query);
+		 pstmt = conn.prepareStatement(query);
+		 pstmt.setInt(1, articleNO);
+		 pstmt.setInt(2, parentNO);
+		 pstmt.setString(3, title);
+		 pstmt.setString(4, content);
+		 pstmt.setString(5, imageFileName);
+		 pstmt.setString(6, id);
+		 pstmt.executeUpdate();
+		 pstmt.close();
+		 conn.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	 
+	 
+   }
+}
