ee635c4c050a332e4ceeebeb421b35a84d27783e
dltkdgksqhr <78789ks@naver.com>
2022-05-07 22:30:57 +0900

    22/05/07  MultiActionController 이용해 회원정보 표시하기

--- pro21/src/com/spring/ex02/UserController.java
+++ pro21/src/com/spring/ex02/UserController.java
@@ -9,17 +9,84 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class UserController extends MultiActionController { // 설정 파일의 userMethodNameResolver 프로퍼티를 사용하려면 반드시 MultiActionController를 상속받아야한다.
- public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
-	String userID=""; 
-	String passwd=""; 
-	ModelAndView mav = new ModelAndView();
-	request.setCharacterEncoding("utf-8");
-	userID = request.getParameter("userID");
-	passwd = request.getParameter("passwd");
+	public ModelAndView memberInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+	    ModelAndView mav=new ModelAndView();
+	    String id=request.getParameter("id");
+	    String pwd=request.getParameter("pwd");
+	    String name=request.getParameter("name");
+	    String email=request.getParameter("email");
+
+	    mav.addObject("id",id);
+	    mav.addObject("pwd",pwd);
+	    mav.addObject("name",name);
+	    mav.addObject("email",email);
+	    mav.setViewName("memberInfo");
+	    return mav;
+	}
 	
-	mav.addObject("userID",userID); //ModelAndView에 로그인 정보를 바인딩합니다
-	mav.addObject("passwd", passwd);
-	mav.setViewName("result"); //ModelAndView 객체에 포워딩할 JSP 이름을 설정합니다.
-	return mav;
- }
+	/*public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String userID = "";
+		String passwd = "";
+		ModelAndView mav = new ModelAndView();
+		request.setCharacterEncoding("utf-8");
+		userID = request.getParameter("userID");
+		passwd = request.getParameter("passwd");
+		mav.addObject("userID", userID);
+		mav.addObject("passwd", passwd);
+		mav.setViewName("result");
+		return mav;
+	}*/
+	
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+		String userID = "";
+		String passwd = "";
+		//String viewName=getViewName(request); // getViewName()메서드를 호출해 요청명에서 확장명.do를 제외한 뷰이름을 가져옵니다.
+		ModelAndView mav = new ModelAndView();
+		request.setCharacterEncoding("utf-8");
+		userID = request.getParameter("userID");
+		passwd = request.getParameter("passwd");
+		String viewName=getViewName(request);
+		
+		mav.addObject("userID", userID);
+		mav.addObject("passwd", passwd);
+		//mav.setView(viewName); 
+		mav.setViewName(viewName);// view이름을 지정합니다.
+		System.out.println("ViewName:" + viewName);
+		return mav;
+	}
+	
+	private String getViewName(HttpServletRequest request) {
+	  String contextPath = request.getContextPath();
+	  String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+	  if(uri == null || uri.trim().equals("")) {
+		 uri = request.getRequestURI(); 
+	  }
+	  
+	  int begin = 0;  //
+      if(!((contextPath==null)||("".equals(contextPath)))){
+         begin = contextPath.length();  // 전체 요청명의 길이를 구함
+      }
+	  
+	  int end;
+	  if(uri.indexOf(";")!=-1) {
+		  end=uri.indexOf(";");
+	  } else if(uri.indexOf("?")!=-1) {
+		  end = uri.indexOf("?");
+	  } else {
+		  end= uri.length();
+	  }
+	  
+	  //http://localhost:8090/member/listMember.do로 요청시 먼저 '.do'를 제거한 http://localhost:8090/member/listMember를 구한 후,
+      //다시 http://localhost:8090/member/listMember에서 역순으로 첫번째 '/' 위치를 구한 후, 그 뒤의 listMember를 구한다.
+      String fileName=uri.substring(begin,end);
+      if(fileName.indexOf(".")!=-1){
+         fileName=fileName.substring(0,fileName.lastIndexOf("."));  //요청명에서 역순으로 최초 '.'의 위치를 구한후, '.do' 앞에까지의 문자열을 구함
+      }
+      if(fileName.lastIndexOf("/")!=-1){
+         fileName=fileName.substring(fileName.lastIndexOf("/"),fileName.length()); //요청명에서 역순으로 최초 '/'의 위치를 구한후, '/' 다음부터의 문자열을 구함  
+      }
+		return fileName;
+		
+	}
 }
